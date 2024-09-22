9776d926a932246fc1a74d0e873a11fd172716b2
3duard89 <dduardidrobo@genstudents.org>
2024-09-22 02:19:30 -0500

    pagina de ingreso usuario o registro

--- scriptlogin.js
+++ scriptlogin.js
@@ -1,16 +1,17 @@
-const loginBtn = document.querySelector(".loginBtn");
-const registroBtn = document.querySelector(".registroBtn");
-const formBox = document.querySelector(".formBox");
-const body = document.querySelector("body");
+const loginBtn = document.querySelector(".ingresoBoton");
+const registroBtn = document.querySelector(".registroBoton");
+
+const contieneFormulario = document.querySelector(".contieneFormularios");
+const body = document.querySelector(".bodyLogin");
 
 
 registroBtn.onclick = function(){
-    formBox.classList.add('active');
+    contieneFormulario.classList.add('active');
     body.classList.add('active');
 }
 
 loginBtn.onclick = function(){
-    formBox.classList.remove('active');
+    contieneFormulario.classList.remove('active');
     body.classList.remove('active');
 }
 
