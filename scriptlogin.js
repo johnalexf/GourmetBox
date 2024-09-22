613c1a36fff654ce2d5971c0dd4223bd36963cd5
3duard89 <dduardidrobo@genstudents.org>
2024-09-22 09:56:54 -0500

    validacion required inputs y redireccionamiento pagina navbar

--- scriptlogin.js
+++ scriptlogin.js
@@ -1,9 +1,15 @@
+
+/* variables para modificar los estilos y acceder a los datos */
 const loginBtn = document.querySelector(".ingresoBoton");
 const registroBtn = document.querySelector(".registroBoton");
 
 const contieneFormulario = document.querySelector(".contieneFormularios");
 const body = document.querySelector(".bodyLogin");
 
+// Variables para manipular ventanas emergentes
+var modalContinuarFormulario = document.getElementById("modalContinuarFormulario");
+var modalMensajeExitoso = document.getElementById("modalMensajeExitoso");
+var cerrarModalExitoso = document.getElementsByClassName("cerrar")[0]; //Esta instrucción se parece a document.querySelectorAll
 
 registroBtn.onclick = function(){
     contieneFormulario.classList.add('active');
@@ -15,3 +21,14 @@ loginBtn.onclick = function(){
     body.classList.remove('active');
 }
 
+
+
+function verificarEmail(){
+    if(!expresiones.correo.test(email.value)){
+      email.setCustomValidity(
+        "¡El correo que ingresaste no es valido!",
+      );
+    } else {
+      email.setCustomValidity("");
+    }
+  }
