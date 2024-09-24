68c9dc7a7dc186619c67e45c467c5f4f31468bd6
John Forero <johnalexfr@gmail.com>
2024-09-23 22:28:23 -0500

    corrigiendo error al momento de volver a los inputs de contraseña, mostraba los mensaje de advertencia asi estuviera cumpliendo con la sentencia

--- scripts/scriptlogin.js
+++ scripts/scriptlogin.js
@@ -6,20 +6,20 @@ const registroBotonCambioVentana = document.querySelector(".registroBotonCambioV
 
 // contieneFormulario contiene los dos formularios de ingreso y de registro
 const contieneFormulario = document.querySelector(".contieneFormularios");
-//bodylogin contiene toda la estructura de formularios de registro y de login
+//bodyLogin contiene toda la estructura de formularios de registro y de login
 const bodyLogin = document.querySelector(".bodyLogin");
 
 // Variables para manipular ventanas emergentes
 const modalMensajeExitoso = document.getElementById("modalMensajeExitoso");
 const cerrarModalExitoso = document.querySelector(".cerrar"); 
 
-// Cuando oprima el boton de registro para cambio de ventana, activa la funcionalidad de los contenedores para hacer el cambio de formularios
+// Cuando oprima el botón de registro para cambio de ventana, activa la funcionalidad de los contenedores para hacer el cambio de formularios
 registroBotonCambioVentana.onclick = function(){
     contieneFormulario.classList.add('active');
     bodyLogin.classList.add('active');
 }
 
-// Cuando oprima el boton de login para cambio de ventana, desactiva la funcionalidad de los contenedores para hacer el cambio de formularios
+// Cuando oprima el botón de login para cambio de ventana, desactiva la funcionalidad de los contenedores para hacer el cambio de formularios
 loginBotonCambioVentana.onclick = function(){
     contieneFormulario.classList.remove('active');
     bodyLogin.classList.remove('active');
@@ -31,14 +31,14 @@ cerrarModalExitoso.onclick = function() {
 }
 
 
-//Codigo para validacion de datos de correo y contraseña
-/* expresiones para validar correo y contrasena */
+//Código para validación de datos de correo y contraseña
+/* expresiones para validar correo y Contrasena */
 const expresiones = {
   correoE: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,3}$/,
   contrasena: /^(?=.*[A-Z])(?=.*[0-9]).{6,}$/
 }
 
-//metodo de escucha para cuando hay cambios en el input de correo y despues de un submit activa el mensaje correspondiente
+//método de escucha para cuando hay cambios en el input de correo y después de un submit activa el mensaje correspondiente
 document.getElementById('correo').addEventListener('input', function(){
   const correo = document.getElementById('correo');
   let validoCorreo = expresiones.correoE.test(correo.value);
@@ -66,73 +66,71 @@ document.getElementById('correo').addEventListener('input', function(){
 let contrasenaRegistro = document.getElementById('contrasenaR');
 let confirmacionContrasenaRegistro = document.getElementById('validarContrasena')
 
-//metodo de escucha de un click sobre los input de contraseña
-window.addEventListener('click', (event) =>{ 
-  if(event.target.id == "contrasenaR"){
-    contrasenaRegistro.reportValidity();
+function verificarContrasenaAdecuada(){
+  if(expresiones.contrasena.test(contrasenaRegistro.value)){
+    contrasenaRegistro.setCustomValidity('');
+   }else{
+    contrasenaRegistro.reportValidity(); //habilita el mensaje de advertencia
     contrasenaRegistro.setCustomValidity(
       'La contraseña debe tener al menos 6 caracteres, una mayúscula y un número.'
     );
-  }
-  if(event.target.id == "validarContrasena"){
-    confirmacionContrasenaRegistro.reportValidity();
-    confirmacionContrasenaRegistro.setCustomValidity(
-      'Las contraseñas no coinciden.'
-    );
-  }
-
-})
-
-//metodo de escucha sobre cambios en el input de contrasenaRegistro para verificar que cumpla la expresion
-contrasenaRegistro.addEventListener('input',function(){
-   if(expresiones.contrasena.test(contrasenaRegistro.value)){
-    contrasenaRegistro.setCustomValidity('');
    }
-})
+}
 
-//metodo de escucha sobre cambios en el input de confirmacionContrasenaRegistro para verificar que cumpla la expresion
-confirmacionContrasenaRegistro.addEventListener('input',function(){
-  console.log("confirmacion")
+function verificarContrasenaIguales(){
   if(confirmacionContrasenaRegistro.value === contrasenaRegistro.value){
-   confirmacionContrasenaRegistro.setCustomValidity('');
+    confirmacionContrasenaRegistro.setCustomValidity('');
+  }else{
+     confirmacionContrasenaRegistro.reportValidity();
+     confirmacionContrasenaRegistro.setCustomValidity(
+       'Las contraseñas no coinciden.'
+     );
   }
+}
+
+//método de escucha de un click sobre los input de contraseña
+window.addEventListener('click', (event) =>{ 
+  if(event.target.id == "contrasenaR"){verificarContrasenaAdecuada();}
+  if(event.target.id == "validarContrasena"){verificarContrasenaIguales();}
 })
 
+//método de escucha sobre cambios en el input de contrasenaRegistro para verificar que cumpla la expresión
+contrasenaRegistro.addEventListener('input', verificarContrasenaAdecuada)
+
+//método de escucha sobre cambios en el input de confirmacionContrasenaRegistro para verificar que cumpla la expresión
+confirmacionContrasenaRegistro.addEventListener('input',verificarContrasenaIguales)
 
 
-
-// para eduard, al momento de activar el mensaje con correo.setCustomValidity("¡El correo que ingresaste no es valido!", ); hasta no cumplir esa condicion
-//no te va dejar hacer submit, entonces ya no seria necesario de la linea 70 a la 78
+// para Edward, al momento de activar el mensaje con correo.setCustomValidity("¡El correo que ingresaste no es valido!", ); hasta no cumplir esa condición
+//no te va dejar hacer submit, entonces ya no seria necesario ponerlo dentro de este evento de escucha
 document.getElementById('formularioR').addEventListener('submit', function(event) {
-   /* no se envia por defecto */
-  event.preventDefault();
-  /* guardo el valor dentro de correo */
-  const correo = document.getElementById('correo').value;
-  /* valido si tiene las caracteristica de un correo */
-  let validoCorreo = expresiones.correoE.test(correo);
+   
+  event.preventDefault(); /* no se envía por defecto */
+  const correo = document.getElementById('correo').value; /* guardo el valor dentro de correo */
+  let validoCorreo = expresiones.correoE.test(correo); /* valido si tiene las característica de un correo */
   
-  /* si el correo es valido cotinua evaluando */
+  /* si el correo es valido continua evaluando */
   if(!validoCorreo){
     mensaje.textContent = 'Correo Incompleto';
     mensaje.style.color = 'red';
   }else{
 
   /* guardo los contenidos de los inputs contrasena y validarContrasena*/
-    const contrasena = document.getElementById('contrasenaR').value;
-    const validarContrasena = document.getElementById('validarContrasena').value;
+    const contrasena = contrasenaRegistro.value;
+    const validarContrasena = confirmacionContrasenaRegistro.value;
     const mensaje = document.getElementById('mensaje');
 
-    /* Valido que la contrasena tenga una mayuscula y un numero y sea de al menos 6 caracteres */
+    /* Valido que la contrasena tenga una mayúscula y un numero y sea de al menos 6 caracteres */
     const esValida = expresiones.contrasena.test(contrasena);
     
-    /* si mi contrasenas con iguales y tienen una mayuscula y un numero y tiene al menos 6 caracteres
-    se envia un mensaje modal dando la Bienvenida */
+    /* si mi contraseñas con iguales y tienen una mayúscula y un numero y tiene al menos 6 caracteres
+    se envía un mensaje modal dando la Bienvenida */
     if (esValida && contrasena == validarContrasena) {
       modalMensajeExitoso.style.display = 'block';
 
     /* en caso que la contrasena no sean iguales que le diga al usuario */
     } else if(contrasena != validarContrasena){
-      mensaje.textContent = 'No Coinciden las Contrasenas';
+      mensaje.textContent = 'No Coinciden las Contraseñas';
       mensaje.style.color = 'red';
     /* en caso que la contrasena no cumpla con las condiciones se le informa al usuario */
     }else{
@@ -151,8 +149,8 @@ document.getElementById('contrasenaR').addEventListener('input', function() {
 
 
 
-// Codigo para cambiar de visualizacion si el usuario hace un correcto login
-//ya hemos llamado al div class bodylogin como body
+// Código para cambiar de visualización si el usuario hace un correcto login
+//ya hemos llamado al div class bodyLogin como body
 let usuarioRegistrado = document.querySelector(".usuarioRegistrado");
 let botonRealizarIngreso = document.querySelector("#realizarIngreso");
 let botonCerrarSesion = document.querySelector("#cerrarSesion");
@@ -167,7 +165,7 @@ if(localStorage.getItem('usuario') != undefined){
   }
 }
 
-//Cuando orpime en ingresar, verifica el usuario y la contraseña, si son correctas muestra el div de perfil y oculta el de formularios de ingreso
+//Cuando oprime en ingresar, verifica el usuario y la contraseña, si son correctas muestra el div de perfil y oculta el de formularios de ingreso
 botonRealizarIngreso.onclick = function(event) {
   event.preventDefault();
   if(formularioIngreso.usuario.value === "admin" && formularioIngreso.contrasena.value === "1234" ){
@@ -178,7 +176,7 @@ botonRealizarIngreso.onclick = function(event) {
   }
 }
 
-//Cuando cierra sesion muestra los formularios y oculta el perfil
+//Cuando cierra sesión muestra los formularios y oculta el perfil
 botonCerrarSesion.onclick = function() {
   bodyLogin.style.display = "flex";
   usuarioRegistrado.style.display = "none";
