ebd6f015f93d3037efe4b6394aa6c2b6b54be2f8
John Forero <johnalexfr@gmail.com>
2024-10-29 08:49:55 -0500

    cambio de estilos de letras poniendo poppi y roboto para todas las paginas

--- scripts/llamarNavFooter.js
+++ scripts/llamarNavFooter.js
@@ -1,14 +1,3 @@
-// // Cargar el contenido del footer
-// fetch('../HTML/navbar.html')
-//     .then(response => response.text())
-//     .then(data => {
-//     document.getElementById('Navbar').innerHTML = data;
-//     console.log(data)
-//     })
-//     .catch(error => console.error('Error al cargar el footer:', error));
-
-
-
 // Cargar el contenido del footer
 fetch('../HTML/footer.html')
     .then(response => response.text())
--- scripts/scriptmenu.js
+++ scripts/scriptmenu.js
@@ -83,7 +83,7 @@ function actualizarMenu() {
             <p>${producto.descripcion}</p>
             </div>
             <div class="contenedorPrecioCard">
-            <p> <b>$  ${producto.precio}  COP</b> </p>
+            <p> <b>$  ${producto.precio.toLocaleString()}  COP</b> </p>
             </div>
             <!-- contenedor boton de la card -->
             <div class="contenedorBotonCard">
@@ -148,9 +148,9 @@ export function adicionarCarrito(id) {
         carritoCantidadAgregadaNavbar();
 
         localStorage.setItem('listaCompras', JSON.stringify(listaCompras));
-        mensajeModal.innerHTML = `<h5> El producto  <b>${menu[indiceMenu].nombre} </b> ha sido agregado al carrito. </h5>`;
+        mensajeModal.innerHTML = `<p> El producto  <b>${menu[indiceMenu].nombre} </b> ha sido agregado al carrito. </p>`;
     } else {
-        mensajeModal.innerHTML = `<h5> El producto  <b>${menu[indiceMenu].nombre} </b> ya se ha agregado al carrito. </h5>`;
+        mensajeModal.innerHTML = `<p> El producto  <b>${menu[indiceMenu].nombre} </b> ya se ha agregado al carrito. </p>`;
     }
 
     modalAgregadoACarrito.style.display = "flex";
